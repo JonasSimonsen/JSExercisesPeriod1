8532853f044a944c751b447e7a1b66c683be3dc2
jonassimonsen <jks.ufo@icloud.com>
2016-02-15 09:45:31 +0100

    Initial commit

--- NodeExercises/3_my_first_io.js
+++ NodeExercises/3_my_first_io.js
@@ -1,3 +1,13 @@
 /**
  * Created by jonassimonsen on 15/02/2016.
  */
+var fs = require('fs')
+
+var contents = fs.readFileSync(process.argv[2])
+var lines = contents.toString().split('\n').length - 1
+console.log(lines)
+
+// note you can avoid the .toString() by passing 'utf8' as the
+// second argument to readFileSync, then you'll get a String!
+//
+// fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
--- NodeExercises/5_filtered_ls.js
+++ NodeExercises/5_filtered_ls.js
@@ -1,3 +1,12 @@
 /**
  * Created by jonassimonsen on 15/02/2016.
  */
+var fs = require('fs')
+var path = require('path')
+
+fs.readdir(process.argv[2], function (err, list) {
+    list.forEach(function (file) {
+        if (path.extname(file) === '.' + process.argv[3])
+            console.log(file)
+    })
+})
