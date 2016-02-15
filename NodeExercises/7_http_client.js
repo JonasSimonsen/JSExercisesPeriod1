0aff56ccafa06f839057c6dfc8e08d88223a68b4
jonassimonsen <jks.ufo@icloud.com>
2016-02-15 10:09:17 +0100

    Initial commit

--- NodeExercises/7_http_client.js
+++ NodeExercises/7_http_client.js
@@ -1,3 +1,10 @@
 /**
  * Created by jonassimonsen on 15/02/2016.
  */
+var http = require('http')
+
+http.get(process.argv[2], function (response) {
+    response.setEncoding('utf8')
+    response.on('data', console.log)
+    response.on('error', console.error)
+})
